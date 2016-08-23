2e0b385e4d21d5a04dac44a14ae229e757ab2585
maprohu <maprohu@gmail.com>
2016-08-23 23:26:01 +0100

    wup bsh

--- src/main/scala/osgi6/admin/exec/AdminExecActivator.scala
+++ src/main/scala/osgi6/admin/exec/AdminExecActivator.scala
@@ -6,7 +6,7 @@ import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 import org.osgi.framework.{Bundle, BundleContext}
 import osgi6.actor.ActorSystemActivator
 import osgi6.akka.slf4j.AkkaSlf4j
-import osgi6.common.{AsyncActivator, HttpTools, OsgiTools}
+import osgi6.common.{AsyncActivator, HttpTools, OsgiAdmin, OsgiTools}
 import osgi6.lib.multi.{ContextApiActivator, MultiApiActivator}
 import osgi6.multi.api.MultiApi.Callback
 import osgi6.multi.api.{Context, ContextApi, MultiApi}
@@ -51,33 +51,7 @@ object AdminExecActivator {
 
     val handler = new MultiApi.Handler {
       override def dispatch(req: HttpServletRequest, resp: HttpServletResponse, callback: Callback): Unit = {
-        def processAdminRequestHtml(fn: => String) =
-          processAdminRequest(fn, "text/html")
 
-        def processAdminRequest(fn: => String, ct: String = "text/plain") = {
-          processAdminRequestStream({ os =>
-            os.write(fn.getBytes)
-          })
-        }
-
-        def processAdminRequestStream(fn: OutputStream => Unit, ct: String = "text/plain") = {
-          resp.setContentType(ct)
-          HttpTools.preResponse(req, resp)
-          val os = resp.getOutputStream
-          try {
-            try {
-              fn(os)
-            } catch {
-              case NonFatal(ex) =>
-                val pw = new PrintWriter(os)
-                ex.printStackTrace(pw)
-                pw.close()
-            }
-          } finally {
-            os.close()
-          }
-          callback.handled(true)
-        }
 
         val rootPath = context.rootPath
         val requestUri = req.getServletPath + Option(req.getPathInfo).getOrElse("")
@@ -92,13 +66,9 @@ object AdminExecActivator {
 
           servletPath match {
             case Some("/_admin/exec") =>
-              processAdminRequestStream { os =>
-                OsgiTools.execBundle(
-                  ctx,
-                  req.getInputStream,
-                  os
-                )
-              }
+              OsgiAdmin.dispatch(ctx, req, resp)
+              callback.handled(true)
+
             case _ =>
               callback.handled(false)
           }
@@ -119,15 +89,4 @@ object AdminExecActivator {
   }
 
 
-  def getStateString(bundle: Bundle) : String = {
-    getStateString(bundle.getState)
-  }
-  def getStateString(state: Int) : String = {
-    if (state == 32) "Active     "
-    else if (state == 2) "Installed  "
-    else if (state == 4) "Resolved   "
-    else if (state == 8) "Starting   "
-    else if (state == 16) "Stopping   "
-    else "Unknown    "
-  }
 }
